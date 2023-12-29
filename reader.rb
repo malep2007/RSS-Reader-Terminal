c2a7a15381ebd933d77cc3e1c1080ed3f3cc88c6
Ephraim Malinga <ephraim.malinga@gmail.com>
2023-12-30 00:14:21 +0300

    User can input rss url and receive feed

--- reader.rb
+++ reader.rb
@@ -4,25 +4,39 @@ require 'rss'
 require 'open-uri'
 
 class RSSReader
+    attr_reader :url, :feed, :title, :description, :link
+
     def initialize(url)
       @url = url
     end
 
-    def fetch(url)
-        URI.open(url) do |rss|
+    def fetch
+        URI.open(@url) do |rss|
             @feed = RSS::Parser.parse(rss)
             @title = @feed.channel.title
-            @feed.items.each do |item|
-                @item = "Item #{item.title} - #{item.pubDate}"
-            end
+            @description = @feed.channel.description
+        end
+    end
+
+    def self.run
+        puts "Enter a URL to an RSS feed: "
+        url = gets.chomp
+        reader = RSSReader.new(url)
+        reader.fetch
+        puts reader.title
+        reader.feed.items.each do |item|
+            puts "---------------------------------"
+            puts "Title: #{item.title}"
+            puts "Description: #{item.description}"
+            puts "Link: #{item.link}"
+            puts "---------------------------------"
         end
     end
 end
 
 
-if __FILE__ == $0
-    reader = RSSReader.new("http://feeds.bbci.co.uk/news/rss.xml")
-    reader.fetch
 
-    puts reader.title
+
+if __FILE__ == $0
+    RSSReader.run
 end
